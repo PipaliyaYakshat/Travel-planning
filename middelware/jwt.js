80a5fc8cf5fb33398fb0dde7485f202a943dd2a3
yakshat <pipaliyayakshat2024.katargam@gmail.com>
2025-04-18 11:03:26 +0530

    final commit 4

--- middelware/jwt.js
+++ middelware/jwt.js
@@ -1,26 +1,32 @@
-exports.Auth = async (req, res, next) => {
+const jwt = require('jsonwebtoken')
+const UM = require('../model/user')
+exports.Auth = async(req, res , next) => {
+    // console.log("Hello");
     try {
-        const token = req.headers.authorization;
+        const token = req.headers.authorization
+        // console.log(token);
 
-        if (!token) throw new Error("Attach Token");
+        if(!token) throw new Error("Attach Token")
 
-        const tokenVerify = jwt.verify(token, process.env.Secure_key);
+        const tokenVerify = jwt.verify(token , process.env.Secure_key)
 
-        const userVerify = await UM.findById(tokenVerify.id);
-        if (!userVerify) throw new Error("Invalid User");
+        if(!tokenVerify) throw new Error("Invalid Token")
 
-        next();
+        const userVerify = await UM.findById(tokenVerify.id)
+        if(!userVerify) throw new Error("Invalid User")
+
+            next()
+
     } catch (error) {
-        let message = error.message;
-
-        // Handle JWT token expiry
-        if (error.name === 'TokenExpiredError') {
-            message = 'Token Expired';
-        }
-
-        res.status(401).json({
-            status: 'fail',
-            message
-        });
+        res.status(404).json({
+            status : 'fail',
+            message : error.message
+        })
     }
-};
+
+
+}
+
+
+
+      
